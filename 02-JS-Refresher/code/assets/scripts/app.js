7568ed42eef16254d2f12885d3e8806d522b6305
Jakub.Dragowski <kuba.dragowski@gmail.com>
2025-05-29 16:58:53 +0200

    Study imports and add ESLint configuration

--- 02-JS-Refresher/code/assets/scripts/app.js
+++ 02-JS-Refresher/code/assets/scripts/app.js
@@ -1,5 +1,11 @@
-import { apiKey } from './util.js';
+// import { apiKey } from './util.js';
 
+// import apiKey from './util.js';
+// import { apiKey, abc } from './util.js';
+// import * as util from './util.js';
+import { apiKey, abc as content } from './util.js';
+
+// console.log(util.apiKey);
+// console.log(abc);
 console.log(apiKey);
-
-console.log('Hello, World!');
+console.log(content);
