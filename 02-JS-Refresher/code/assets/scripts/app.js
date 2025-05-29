d07c5c3d02f8fec0a19a50e88da344edfca3a955
Jakub.Dragowski <kuba.dragowski@gmail.com>
2025-05-29 17:22:05 +0200

    Revisiting Variables & Value + Operators

--- 02-JS-Refresher/code/assets/scripts/app.js
+++ 02-JS-Refresher/code/assets/scripts/app.js
@@ -1,11 +1,39 @@
+// 1. "Import" & "Export"
+import { apiKey, abc as content } from './util.js';
 // import { apiKey } from './util.js';
-
 // import apiKey from './util.js';
+
 // import { apiKey, abc } from './util.js';
 // import * as util from './util.js';
-import { apiKey, abc as content } from './util.js';
 
 // console.log(util.apiKey);
 // console.log(abc);
 console.log(apiKey);
 console.log(content);
+
+// 2. Revisiting Variables & Value
+const userMessage = `Hello Jacob!!! It's a great day on horizon!`;
+// ⬆️
+// You must used let to change the value of a variable later
+// userMessage = `Change the message to something else!`;
+
+console.log(userMessage);
+console.log(userMessage);
+
+function createGreeting(userName, message = 'Hello!') {
+  console.log(userName);
+  console.log(message);
+  return `Hi, I am ${userName}. ${message}`;
+}
+
+const greeting1 = createGreeting('Jacob');
+console.log(greeting1);
+
+// 3. Revisiting Operators
+const a = 10;
+const b = 5 + 5;
+if (a === b) {
+  console.log('works');
+}
+
+// 4. Functions
