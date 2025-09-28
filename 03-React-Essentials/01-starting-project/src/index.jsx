ccdd26991997f7717c756e0bfb47f0a61f35a536
Jakub.Dragowski <kuba.dragowski@gmail.com>
2025-09-28 21:43:10 +0200

    #First custom component

--- 03-React-Essentials/01-starting-project/src/index.jsx
+++ 03-React-Essentials/01-starting-project/src/index.jsx
@@ -1,7 +1,15 @@
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 import App from './App.jsx';
 import './index.css';
 
-const entryPoint = document.getElementById('root');
-ReactDOM.createRoot(entryPoint).render(<App />);
+// const entryPoint = document.getElementById('root');
+// ReactDOM.createRoot(entryPoint).render(<App />);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
